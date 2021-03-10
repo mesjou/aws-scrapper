63e990b0080e977bdec30eb63565716f6e062023
matthias-hettich <matthias.hettich@outlook.de>
2021-03-10 12:35:30 +0100

    Reformatted scraper.py

--- src/scraper.py
+++ src/scraper.py
@@ -4,7 +4,6 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-# http://www.networkinghowtos.com/howto/common-user-agent-list/
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) \
     Version/14.0.3 Safari/605.1.15",
@@ -12,94 +11,127 @@ HEADERS = {
 }
 
 
-def search_product_list():
+def search_product_list(tracker_path, result_path):
     """
-    This function lods a csv file named TRACKER_PRODUCTS.csv, with headers: [url, code, buy_below]
-    It looks for the file under in ./trackers
-
-    It also requires a file called SEARCH_HISTORY.xslx under the folder ./search_history to start saving the results.
-    An empty file can be used on the first time using the script.
-
-    Both the old and the new results are then saved in a new file named SEARCH_HISTORY_{datetime}.xlsx
-    This is the file the script will use to get the history next time it runs.
+    Load tracker file and execute scrape function for all rows and save to output file.
 
     Parameters
     ----------
-    interval_count : TYPE, optional
-        DESCRIPTION. The default is 1. The number of iterations you want the script to run a search on the full list.
-    interval_hours : TYPE, optional
-        DESCRIPTION. The default is 6.
+    tracker_path: path to csv file that stores all products to scrape
+    result_path: path to results csv to store scrapping results to
 
     Returns
     -------
-    New .xlsx file with previous search history and results from current search
-
+    None
     """
-    prod_tracker = pd.read_csv("trackers/tracker.csv", sep=";")
-    now = datetime.now().strftime("%Y-%m-%d %H:%M")
-    search_hist = pd.read_csv("search_history/search_results.csv")
+    prod_tracker = pd.read_csv(tracker_path, sep=";")
+    search_hist = pd.read_csv(result_path, sep=";")
 
     for row in prod_tracker.itertuples():
-        page = requests.get(row.url, headers=HEADERS)
-        soup = BeautifulSoup(page.content, features="lxml")
+        search_hist = search_hist.append(scrape_infos(row.url, row.code, row.Index), sort=False)
 
-        # find product name
-        try:
-            title = soup.find(id="productTitle").get_text().strip()
-        except:  # noqa E722
-            title = ""
+    search_hist.to_csv(result_path, index=False, sep=";")
 
-        # find product price
-        try:
-            price = float(soup.find(id="priceblock_ourprice").get_text().replace("€", "").replace(",", ""))
-        except:  # noqa E722
-            price = ""
 
-        # find review
-        try:
-            review_score = float(
-                soup.select('i[class*="a-icon a-icon-star a-star-"]')[0].get_text().split(" ")[0].replace(",", ".")
-            )
-            review_count = int(soup.select("#acrCustomerReviewText")[0].get_text().split(" ")[0].replace(".", ""))
-        except:  # noqa E722
-            try:
-                review_score = float(
-                    soup.select('i[class*="a-icon a-icon-star a-star-"]')[1].get_text().split(" ")[0].replace(",", ".")
-                )
-                review_count = int(soup.select("#acrCustomerReviewText")[0].get_text().split(" ")[0].replace(".", ""))
-            except:  # noqa E722
-                review_score = ""
-                review_count = ""
+def scrape_infos(url, code, i):
+    """
+    Scrape informations from amazon website using soup
 
-        # check availability
+    Parameters
+    ----------
+    url : the url of the amazon website that should be scraped
+    code : product code
+
+    Returns
+    -------
+    pandas data frame with this columns:
+        "date"
+        "code"
+        "url"
+        "title"
+        "price"
+        "stock"
+        "review_score"
+        "review_count"
+    """
+
+    page = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(page.content, features="lxml")
+
+    return pd.DataFrame(
+        {
+            "date": datetime.now().strftime("%Y-%m-%d %H:%M"),
+            "code": code,
+            "url": url,
+            "title": scrape_name(soup),
+            "price": scrape_price(soup),
+            "stock": scrape_stock(soup),
+            "review_score": scrape_review_score(soup),
+            "review_count": scrape_review_count(soup),
+        },
+        index=[i],
+    )
+
+
+def scrape_name(soup_object):
+    try:
+        name = soup_object.find(id="productTitle").get_text().strip()
+    except:  # noqa E722
+        name = ""
+    return name
+
+
+def scrape_price(soup_object):
+    try:
+        price = float(soup_object.find(id="priceblock_ourprice").get_text().replace("€", "").replace(",", ""))
+    except:  # noqa E722
+        price = ""
+    return price
+
+
+def scrape_stock(soup_object):
+    try:
+        soup_object.select("#availability .a-color-state")[0].get_text().strip()
+        stock = "Out of Stock"
+    except:  # noqa E722
         try:
-            soup.select("#availability .a-color-state")[0].get_text().strip()
+            soup_object.select("#availability .a-color-price")[0].get_text().strip()
             stock = "Out of Stock"
         except:  # noqa E722
-            try:
-                soup.select("#availability .a-color-price")[0].get_text().strip()
-                stock = "Out of Stock"
-            except:  # noqa E722
-                stock = "Available"
+            stock = "Available"
+    return stock
 
-        log = pd.DataFrame(
-            {
-                "date": now,
-                "code": row.code,
-                "url": row.url,
-                "title": title,
-                "price": price,
-                "stock": stock,
-                "review_score": review_score,
-                "review_count": review_count,
-            },
-            index=[row.Index],
+
+def scrape_review_score(soup_object):
+    try:
+        review_score = float(
+            soup_object.select('i[class*="a-icon a-icon-star a-star-"]')[0].get_text().split(" ")[0].replace(",", ".")
         )
-        print(log)
-        search_hist = search_hist.append(log, sort=False)
+    except:  # noqa E722
+        try:
+            review_score = float(
+                soup_object.select('i[class*="a-icon a-icon-star a-star-"]')[1]
+                .get_text()
+                .split(" ")[0]
+                .replace(",", ".")
+            )
+        except:  # noqa E722
+            review_score = ""
+    return review_score
 
-    search_hist.to_csv("search_history/search_results.csv".format(now), index=False)
+
+def scrape_review_count(soup_object):
+    try:
+        review_count = int(soup_object.select("#acrCustomerReviewText")[0].get_text().split(" ")[0].replace(".", ""))
+    except:  # noqa E722
+        try:
+            review_count = int(
+                soup_object.select("#acrCustomerReviewText")[0].get_text().split(" ")[0].replace(".", "")
+            )
+        except:  # noqa E722
+            review_count = ""
+    return review_count
 
 
 if __name__ == "__main__":
-    search_product_list()
+    search_product_list("trackers/tracker.csv", "search_history/search_results.csv")
